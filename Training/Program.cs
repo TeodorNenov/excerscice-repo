3eb1bb458195c38e8ff6385af914a9d37e719f9d
Теодор Ненов <teodornenov98@gmail.com>
2017-09-26 19:08:00 +0300

    Add my work

---
+++ Training/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+            if (minutes > 60)
+            {
+                if (hours <23)
+                {
+                    int minutesLeft = minutes - 60;
+                    hours += 1;
+                    Console.WriteLine($"{hours}:{minutesLeft}");
+                }
+
+
+            }
+            else
+            {
+                hours = 0;
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+
+
+        }
+    }
+}
